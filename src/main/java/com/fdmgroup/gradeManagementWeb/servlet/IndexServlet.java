790515da039a0fe8af8a8cd5d21dbc7f5cf2ebc0
trainee <trainee1@LAPSHA00008.fdmgroup.local>
2018-11-09 16:13:38 +0800

    add a new servlet java file and change for the old one

--- src/main/java/com/fdmgroup/gradeManagementWeb/servlet/IndexServlet.java
+++ src/main/java/com/fdmgroup/gradeManagementWeb/servlet/IndexServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class LoginServlet extends HttpServlet{
+public class IndexServlet extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,24 +21,7 @@ public class LoginServlet extends HttpServlet{
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String username = req.getParameter("username");
-		String password = req.getParameter("password");
-		req.setAttribute("lucky_numbers", new int[] {1,2,3,4});
 		
-		//better to call a login service class here
-		RequestDispatcher rd = null;
-		if(username.equals("aa")||password.contains("3")) {
-			//block login
-			rd=req.getRequestDispatcher("block.jsp");
-		}else {
-			//allow login
-			rd=req.getRequestDispatcher("home.jsp");
-			
-			HttpSession session = req.getSession();
-			session.setAttribute("logged_in_user", username);
-		}
-		
-		rd.forward(req, resp);
 		
 	}
 
